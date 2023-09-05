c3db2f360909a42631d709e3c5648c2a7165a94b
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-05 21:53:14 +0000

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(65, 91)
-    print("{}.format(chr(i)))
+for i in range(65, 91):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print("{}.format(chr(i), end=''))
