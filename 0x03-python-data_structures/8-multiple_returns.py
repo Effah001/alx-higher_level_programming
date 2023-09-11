d6a40995a79201cf5105fd89d583828a3c4dc5d7
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-11 19:34:22 +0000

    Create 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+
+def multiple_returns(sentence):
+    if sentence == []:
+        sentence[0] = None
+        first = sentence[0]
+        length = len(sentence)
+    else:
+        length = len(sentence)
+        first = sentence[0]
+    print("{:d} - First character: {}".format(length, first))
