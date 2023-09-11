1ce8527fdeeb69c659e0e51429bcbc43b67ac8ae
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-11 20:13:38 +0000

    Create 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,6 @@
+def divisible_by_2(my_list=[]):
+    for num in my_list:
+        if num % 2 == 0
+            print("{:d} is divisible by 2".format(num))
+        else:
+            print("{:d} is not divisible by 2".format(num))
