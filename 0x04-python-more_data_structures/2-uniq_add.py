02cb93e5b26396e322e27e1bba76792c25d90ae3
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-13 13:34:31 +0000

    Create 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+def uniq_add(my_list=[]):
+    result = 0
+    uniq_set = set()
+    
+    for i in my_list:
+        if i not in unique_set:
+            result += i
+            uniq_set.add(i)
+            
+            print("Result: {:d}".format(result))
