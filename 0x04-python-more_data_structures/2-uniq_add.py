43aa55a622f2a09828e61bd9947cf11536937e9c
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-13 14:23:26 +0000

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,10 +2,10 @@
 def uniq_add(my_list=[]):
     result = 0
     uniq_set = set()
-    
+
     for i in my_list:
         if i not in uniq_set:
             result += i
             uniq_set.add(i)
-            
+
     return result
