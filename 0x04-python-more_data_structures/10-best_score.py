bb701cb6c9bc1c47fb5d10dd0774d256660f9624
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-13 21:39:30 +0000

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
         high_score = None
         for key, value in a_dictionary.items():
-            if value > high_score:
+            if high_score is None or value > high_score:
                 high_score = value
         return high_score
