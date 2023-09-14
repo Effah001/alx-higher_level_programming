cb219bb9712858176cce653cec958058690aabee
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-14 06:25:01 +0000

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-   num = 0
-   uniq = set(my_list)
-   for item in uniq:
+    num = 0
+    uniq = set(my_list)
+    for item in uniq:
         num += item
-   return num
+    return num
