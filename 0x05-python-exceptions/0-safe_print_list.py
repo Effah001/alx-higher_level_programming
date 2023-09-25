a128b3fcc4a786f166df68699a64bb2cc171861b
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-25 13:38:11 +0000

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+	i = 0
+	while i <= x:
 	try:
-		i = 0
-		while i <= x:
-			print(str(my_list[i]))
+		print(str(my_list[i]))
 			i += 1
 		return i
 	except (RangeError):
