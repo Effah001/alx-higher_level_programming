b60002615193acb1e7149eb18d0e4fd9eaa8bf67
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-25 15:42:06 +0000

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ def safe_function(fct, *args):
   try:
       err = fct(*args)
     except BaseException as exp:
-        print(f"Exception: {exp}", file=sys.stderr)
+        print(f"Exception: {exp}", file=stderr)
         return None
     finally:
         return err
