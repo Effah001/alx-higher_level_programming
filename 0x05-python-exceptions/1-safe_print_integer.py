23ae6cf68ae92969873357e77676ba0614cc29f2
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-25 16:57:35 +0000

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-	try:
-		print("{:d}".format(value))
-	except Exception:
-		return False
-	return True
+    try:
+        print("{:d}".format(value))
+    except BaseException:
+        return False
+    else:
+        return True
