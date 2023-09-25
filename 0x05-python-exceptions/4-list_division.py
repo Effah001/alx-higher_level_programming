e4fea243127e9d88d0062cac5724cbc08b54cb9e
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-25 15:00:42 +0000

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,7 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             a = 0
             print("dividing by 0")
-        except (TypeError, ValueError):
+        except TypeError:
             print("Wrong type")
         except IndexError:
             print("Out of range")
