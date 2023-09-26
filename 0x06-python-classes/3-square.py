7e46bcb7234ee05eac8ec7f6e41964ebcce2962b
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-26 15:16:53 +0000

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,10 +7,9 @@ class Square:
     """initializes a square"""
 
     def __init__(self, size=0):
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
