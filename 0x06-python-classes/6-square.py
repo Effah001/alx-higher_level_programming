3ce74487742d9a24d4fcddb09d16410f2e83c1e4
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-26 15:54:20 +0000

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -30,11 +30,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for x in range(self.__position[1]):
+            for a in range(self.__position[1]):
                 print()
-            for i in range(self.__size):
+            for x in range(self.__size):
                 print(" " * self.__position[0], end="")
-                for j in range(self.__size):
+                for y in range(self.__size):
                     print("#", end="")
                 print()
 
