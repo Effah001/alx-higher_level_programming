b4ea2feca381300214545123f9c78abf74098b6f
Droid <44108804+Effah001@users.noreply.github.com>
2023-09-26 15:04:42 +0000

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 
-"""Create a class called square"""
+"""Create a Square class"""
 
 
 class Square:
-    """initializes a square"""
+    """Create a Square blueprint"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
-        return (self.__size * self.__size)
+        return self.__size ** 2
