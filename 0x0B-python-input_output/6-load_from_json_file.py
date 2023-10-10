22aaad96762d78eca357897d9c081c998d0b1cfa
Droid <44108804+Effah001@users.noreply.github.com>
2023-10-10 16:28:09 +0000

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,9 +3,6 @@ import json
 
 
 def load_from_json_file(filename):
-    """
-        Writes the JSON representation of 
-        an object to a file
-    """
-    with open(filename, 'w') as f:
+    """Writes the JSON representation of an object to a file"""
+    with open(filename, 'r') as f:
         return json.loads(f)
