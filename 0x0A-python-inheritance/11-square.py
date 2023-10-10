e75e3bb2034b1b0d4bdb21fc54efd31f028ed880
Droid <44108804+Effah001@users.noreply.github.com>
2023-10-10 10:44:11 +0000

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -12,8 +12,8 @@ class Square(Rectangle):
  a class Square inheritance from BaseGeometry
     """
     def __init__(self, size):
-        self.__size = size
         self.integer_validator("size", size)
+        self.__size = size
 
     def area(self):
         return self.__size * self.__size
