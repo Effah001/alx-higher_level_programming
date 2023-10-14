e0831295b7bc069c9582027e3b796d664843c748
Droid <44108804+Effah001@users.noreply.github.com>
2023-10-14 07:47:12 +0000

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,48 +18,56 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Get the width value"""
         return self.__width
 
     @width.setter
     def width(self, width):
+        """Set the width value"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise TypeError("width must be greater than zero")
+            raise ValueError("width must be greater than zero")
         self.__width = width
 
     @property 
     def height(self):
+        """Get the height"""
         return self.__height
 
     @height.setter
     def height(self, height):
+        """Set the width value"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
-            raise TypeError("height must be greater than zero")
+            raise ValueError("height must be greater than zero")
         self.__height= height
 
     @property
     def x(self):
+        """Get the x value"""
         return self.__x
 
     @x.setter
     def x(self, x):
+        """Set the x value"""
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
-            raise TypeError("x must be greater than zero")
+            raise ValueError("x must be greater than zero")
         self.__x = x
 
     @property
     def y(self):
+        """Get the y value"""
         return self.__y
 
     @y.setter
     def y(self, y):
+        """Set the y value"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value <0:
-            raise TypeError("y must be greater than zero")
+            raise ValueError("y must be greater than zero")
         self.__y = y
