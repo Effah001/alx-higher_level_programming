67f8916727d4069da413478612fd8e014f93ae86
Droid <44108804+Effah001@users.noreply.github.com>
2023-10-16 20:43:59 +0000

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,6 +24,6 @@ class Base:
     def to_json_string(list_dictionaries):
         """converts a list of dictionary to JSON string."""
         if list_dictionaries is None or not isinstance(list_dictionary, list):
-			return []
+			return "[]"
 		return json.dumps(list_dictionaries)
             
