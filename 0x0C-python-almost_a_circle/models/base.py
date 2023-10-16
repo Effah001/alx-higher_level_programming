4d0a9e0c376b0a38f77ce6b1e6b154aacbf5e3e8
Kali <effahfrancis210@gmail.com>
2023-10-16 15:46:52 -0400

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,10 @@ The script for our base class
 
 
 class Base:
-    """ Create base class and assign a public
+    """Create base class and assign a public
     instance attribute
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -24,6 +25,5 @@ class Base:
     def to_json_string(list_dictionaries):
         """converts a list of dictionary to JSON string."""
         if list_dictionaries is None or list_dictionaries == []:
-		return "[]"
-	return json.dumps(list_dictionaries)
-            
+            return "[]"
+        return json.dumps(list_dictionaries)
