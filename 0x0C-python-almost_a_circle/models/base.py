604660ae6634a160f510fbe7febee1d554462b71
Droid <44108804+Effah001@users.noreply.github.com>
2023-10-16 21:26:17 +0000

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,13 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write JSON stirng to a file"""
+        file_name = cls.__name__ + ".json"
+        j_list = []
+        if j_list is not None:
+            j_list = [obj.to_dictionary() for obj in list_objs]
+        with open(file_name, "w", encoding="utf-8") as dt:
+            dt.write(cls.to_json_string(j_list))
