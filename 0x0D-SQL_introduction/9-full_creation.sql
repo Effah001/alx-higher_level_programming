609b4b869ee3ed9bd28d85fdd32e7ccd89edd401
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-14 15:39:16 +0000

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,9 @@
 -- a script that creates a table second_table in the database and passes arguments to it
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
 
 INSERT INTO second_table (id, name) VALUES
     (1, "John", 10),
