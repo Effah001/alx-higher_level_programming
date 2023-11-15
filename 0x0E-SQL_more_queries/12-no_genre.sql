ad53fb2e3a7ebc0320c31876b9f55aa94ae1ae06
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-15 22:22:52 +0000

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
-gg-- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked
+-- a script that lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT ts.title, tvg.genre_id
 FROM tv_shows AS ts LEFT JOIN tv_shows_genre AS tvg
 ON ts.id = tvg.show_id
