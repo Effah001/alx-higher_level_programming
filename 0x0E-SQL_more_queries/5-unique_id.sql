01bb6695cacfbd9b7d43768e5d52531792140c31
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-15 14:42:34 +0000

    Update 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Create the table 'force_name' if not exist
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256) UNIQUE (id));
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1, name VARCHAR(256) UNIQUE (id));
