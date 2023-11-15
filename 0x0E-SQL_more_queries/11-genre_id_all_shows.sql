18a6fa34d7630f55f6c4d5e9734708978ad5d574
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-15 19:41:35 +0000

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 --  a script that lists all shows contained in the database
 SELECT s.title, tvg.genre_id
-FROM tv_shows s LEFT OUTER JOIN tv_show_genres tvg
+FROM tv_shows AS s LEFT OUTER JOIN tv_show_genres AS tvg
 ON s.id = tvg.show_id
 ORDER  BY s.title, tvg.genre_id;
