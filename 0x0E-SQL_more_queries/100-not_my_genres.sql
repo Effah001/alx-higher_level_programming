2afd1562ece1bd8d907d5ba0ae2c251be93b20f1
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-15 21:16:53 +0000

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@ SELECT DISTINCT tvg.name
 FROM tv_genres tvg
 INNER JOIN tv_show_genres tsg
 ON tvg.id = tsg.genre_id
-WHERE tvg.id NOT IN(SELECT tsg.genre_id
+WHERE tvg.id NOT IN (SELECT tsg.genre_id
                     FROM tv_show_genres tsg INNER JOIN tv_shows ts 
                     ON ts.id = tsg.show.id
                     WHERE ts.title = 'Dexter')
