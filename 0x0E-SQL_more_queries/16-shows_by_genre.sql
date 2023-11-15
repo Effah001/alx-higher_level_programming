c5894e68321b2acc1c6eef5a12541295c53de335
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-15 20:52:55 +0000

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT ts.title, tvg.name
 FROM tv_shows ts
 LEFT OUTER JOIN tv_show_genres tsg
-ON s.id = tsg.show_id
+ON ts.id = tsg.show_id
 LEFT OUTER JOIN tv_genres tvg
 ON tvg.id = tsg.genre_id
-ORDER BY ts.title, g.name;
+ORDER BY ts.title, tvg.name;
