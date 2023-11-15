2a2207482e1083408f3821b38f717661a90464ca
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-15 15:12:58 +0000

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                                              state_id INT NOT NULL FOREIGN KEY (states_id) REFERENCES states(id),
+                                              state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES states(id),
                                               name VARCHAR(256) NOT NULL);
