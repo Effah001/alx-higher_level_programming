59731cb8cfef0d65106921a78e3d84c6d09b205d
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-15 20:38:02 +0000

    Create 16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- a script that lists all shows, and all genres linked to that show
+SELECT ts.title, tvg.name
+FROM tv_shows ts
+LEFT OUTER JOIN tv_show_genres tsg
+ON s.id = tsg.show_id
+LEFT OUTER JOIN tv_genres tvg
+ON tvg.id = tsg.genre_id
+ORDER BY ts.title, g.name;
