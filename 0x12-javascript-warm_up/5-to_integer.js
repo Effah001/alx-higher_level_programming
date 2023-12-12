bb0769c029b197f4ec9d724bb750b700af4359e2
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-12 04:31:17 +0000

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node 
+
+const arg = process.argv[2]
+
+const integerValue = parseInt(arg, 10);
+
+if (isNaN(integerValue)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', integerValue);
+}
