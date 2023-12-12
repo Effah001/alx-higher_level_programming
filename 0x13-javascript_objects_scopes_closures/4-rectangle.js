cc6e36031139f1bdbb42862a12880880d3d4701b
effah001 <effahfrancis210@gmail.com>
2023-12-11 19:45:30 -0500

    update

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let rectangle = '';
+    for (let i = 0; i < this.height; i++) {
+      rectangle += 'x'.repeat(this.width);
+      if (i < this.height - 1) {
+        rectangle += '\n';
+      }
+    }
+    console.log(rectangle);
+  }
+
+  rotate () {
+  let temp = '';
+  temp = this.width;
+  this.width = this.height;
+  this.height = temp;
+  }
+
+  double () {
+  this.width *= 2;
+  this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
