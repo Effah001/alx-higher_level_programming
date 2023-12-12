9f1006254314bd04f5541427f0da1dccf8333eb6
effah001 <effahfrancis210@gmail.com>
2023-12-12 00:16:52 -0500

    update

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  for (let i = 0; i < list.length/2; i++) {
-	  let temp = list[i];
-	  list[i] = list[list.length - 1 - i];
-	  list[list.length - 1 - i] = temp;
-    }
-	return list;
-}
+  for (let i = 0; i < list.length / 2; i++) {
+    const temp = list[i];
+    list[i] = list[list.length - 1 - i];
+    list[list.length - 1 - i] = temp;
+  }
+  return list;
+};
