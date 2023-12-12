3a0e1b2eae0e93ff27954265b7b83222af082fc7
effah001 <effahfrancis210@gmail.com>
2023-12-11 23:50:28 -0500

    update

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const Base = require('./5-square');
+
+class Square extends Base {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
