179c5d79333adb5eeadf01a88e03d9c74f197a37
effah001 <effahfrancis210@gmail.com>
2023-12-13 07:50:50 -0500

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,12 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 
 const size = process.argv[2];
 
 if (!isNaN(size) && size > 0) {
-  let repeats = 'X'.repeat(size);
+  const repeats = 'X'.repeat(size);
   for (let i = 0; i < size; i++) {
     console.log(repeats);
   }
 } else {
   console.log('Missing size');
 }
-
