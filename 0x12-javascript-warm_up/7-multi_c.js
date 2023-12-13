f40914dd7f3718ec113226ed520d7031199ed36e
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-13 14:36:14 +0000

    Update 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const x = process.argv[2];
+
+if (!isNaN(x) && x > 0) {
+  let repeats = 'C is fun '.repeat(x);
+  console.log(repeats);
+} else {
+  console.log('Missing number of occurrences');
+}
