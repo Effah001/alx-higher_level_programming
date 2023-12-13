57e558b29d4b130d77d5f340be35bf2de9280c4c
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-13 13:40:36 +0000

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let languages = ['C is fun', 'Python is cool', 'Javascript is Amazing'];
+let languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < languages.length; i++) {
     for (let j = 0; j < languages[i].length; j++) {
