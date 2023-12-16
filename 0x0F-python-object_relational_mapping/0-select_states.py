84be2dc77e771354b35378754f4595c845cedb98
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-16 13:18:50 +0000

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,14 +7,12 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    
-"""
-Access the database and get the states
-""" 
     user_name = sys.argv[1]
     pass_word = sys.argv[2]
-    db_name = sys.argv[3]
-
+    db_name = sys.argv[3]    
+    """
+    Access the database and get the states
+    """ 
     my_db = MySQLdb.connect(host="localhost", port=3306, user=user_name, passwd=pass_word, db=db_name)
 
     cs = my_db.cursor()
