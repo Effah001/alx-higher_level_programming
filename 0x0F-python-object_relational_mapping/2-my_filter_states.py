47197399d43452468f033048a7d2dc5715a56aea
effah001 <effahfrancis210@gmail.com>
2023-12-16 09:39:50 -0500

    update

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+ a script that lists all states with
+ a name starting with N (upper N)
+ from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    pass_word = sys.argv[2]
+    db_name = sys.argv[3]
+    input_name = sys.argv[4]
+
+    """
+    Access the database and get the states
+    """
+
+    my_db = MySQLdb.connect(
+            host="localhost", port=3306, user=user_name, passwd=pass_word, db=db_name)
+
+    cs = my_db.cursor()
+
+    cs.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY states.id ASC", (input_name + '%',))
+
+    rows = cs.fetchall()
+
+    for row in rows:
+        print(row)
