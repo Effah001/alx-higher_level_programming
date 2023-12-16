667adf391194ed1bde7b08172b739235a8af4f40
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-16 13:15:26 +0000

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,14 @@
 a script that lists all states from the database hbtn_0e_0_usa
 """
 
-
 import MySQLdb
-import sys
-
-"""
-Acess the database and get the states
-"""
+from sys import argv
 
 if __name__ == "__main__":
+    
+"""
+Access the database and get the states
+""" 
     user_name = sys.argv[1]
     pass_word = sys.argv[2]
     db_name = sys.argv[3]
