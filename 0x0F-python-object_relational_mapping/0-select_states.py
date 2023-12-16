54cce8e6eb54f3b9ff2c117e1627580f8a91d06a
effah001 <effahfrancis210@gmail.com>
2023-12-16 07:35:59 -0500

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ import sys
 
 
     user_name = sys.argv[1]
-    pass_word sys.argv[2]
+    pass_word = sys.argv[2]
     db_name = sys.argv[3]
 
     my_db = MySQLdb.connect(host="localhost", port=3306, user=user_name, passwd=pass_word, db=db_name)
@@ -26,4 +26,4 @@ import sys
     cs.close()
     db.close()
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
