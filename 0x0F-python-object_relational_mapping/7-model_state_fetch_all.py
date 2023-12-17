d9193394dcf5495ce08f935e2971f85a80a91f73
Droid <44108804+Effah001@users.noreply.github.com>
2023-12-17 19:19:47 +0000

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 """
-a script that lists all states
-from the database hbtn_0e_6_usa
+Module Doc
 """
-
+import sys
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import State, Base
-import sys
 
-if __name__ == "__main__":
-    """
-    Access the database and get the states
-    """
 
-    dburl = "mysql://{}:{}@localhost:3306/{}".format(*sys.argv[1:4])
-
-    engine = create_engine(dburl)
-
-    Session = sessionmaker(bind=engine)
-
+def main():
+    Session = sessionmaker()
+    engine = create_engine(
+        "mysql://{}:{}@localhost/{}".format(*sys.argv[1:4]), pool_pre_ping=True
+    )
+    Session.configure(bind=engine)
     session = Session()
-
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print(f"{state.id}: {state.name}")
+
+
+if __name__ == "__main__":
+    main()
