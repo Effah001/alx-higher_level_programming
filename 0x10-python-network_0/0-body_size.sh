4418b66c242594e3b8538dd7ee2aa4644a84cf30
Droid <44108804+Effah001@users.noreply.github.com>
2024-01-05 14:05:40 +0000

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a Bash script that displays the size of the body of the response
-size=$(curl -s -w '%{size_download}' -o /dev/null https://www.example.com); echo -e "$size"
+size=$(curl -s -w '%{size_download}' -o /dev/null $@); echo -e "$size"
