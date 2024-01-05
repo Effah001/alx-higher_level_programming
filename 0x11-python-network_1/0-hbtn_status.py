0c473eb6d7147bdc5bf5d061edc2831196862ef2
Effah001 <effah210@gmail.com>
2024-01-05 23:07:05 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import urllib.request
+
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-	html = response.read()
+    html = response.read()
+    print(html.decode('utf-8'))
