682b802a4959660250ed2f4fe270c261556fbd1d
Droid <44108804+Effah001@users.noreply.github.com>
2024-01-06 11:58:46 +0000

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display all HTTP allowed on the server
-curl -sI $@ | grep 'Allow' | sed 's/Allow: //'
+# Display all HTTP methods accepted by the server
+curl -sI -X OPTIONS "$1" | grep -i allow | awk '{print $2}'
