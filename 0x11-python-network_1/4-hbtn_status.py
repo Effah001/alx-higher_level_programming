0cdd2d721a5cd81f3e945a86a6650c49808e137f
Droid <44108804+Effah001@users.noreply.github.com>
2024-01-07 16:29:44 +0000

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Get status"""
-import urllib.request
+import requests
 
 
 def main():
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
-        html = resp.read()
+    content = requests.get('https://alx-intranet.hbtn.io/status')
+    html = content.text
         print("Body response:\n\t- type:", type(html.decode('utf-8')))
         print("\t- content:", html.decode('utf-8'))
 
