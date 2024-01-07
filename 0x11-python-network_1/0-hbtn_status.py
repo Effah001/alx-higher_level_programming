7fd708a4237b39aaefe99bd4d26ccd90e9911d45
Droid <44108804+Effah001@users.noreply.github.com>
2024-01-07 14:05:53 +0000

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import urllib.request
 
+def main():
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
 print("Body response:")
 print("    - type:", type(html))
 print("    - content:", html)
 print("    - utf8 content:", html.decode('utf-8'))
+
+if __name__ == "__main__":
+    main()
