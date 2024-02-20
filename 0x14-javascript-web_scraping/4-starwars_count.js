20bafe57ddad74deced3e4b344f29bf1207624a8
Effah001 <effah210@gmail.com>
2024-02-20 13:25:57 +0000

    update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, function(error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response && response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    const id = 18;
+    const filmsWithId = films.filter(film => film.episode_id === id);
+    console.log(filmsWithId.length);
+  } else {
+    console.error('Failed to fetch film details:', response && response.statusCode);
+  }
+});
 
