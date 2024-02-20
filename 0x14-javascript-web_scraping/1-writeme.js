dd88dfba7b34a982aab31354ddefdaf2935df1aa
Effah001 <effah210@gmail.com>
2024-02-20 13:10:14 +0000

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Extract the file path and content from command-line arguments
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Write to the file asynchronously
+fs.writeFile(filePath, content, 'utf8', function(err) {
+  if (err) {
+    console.error('Error writing to file:', err);
+    return;
+  }
+  console.log('Data has been written to the file successfully.');
+});
 
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+request(apiUrl, function(error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  
+  if (response && response.statusCode === 200) {
+    const film = JSON.parse(body);
+    console.log(film.title);
+  } else {
+    console.error('Failed to fetch film details:', response && response.statusCode);
+  }
+});
 
