3a5f8eac99f811590a6f378f7085d75c43c9fe99
Droid <44108804+Effah001@users.noreply.github.com>
2024-02-21 04:40:00 +0000

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,23 @@
+#!/usr/bin/node
 
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function(error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response && response.statusCode === 200) {
+    // Write the contents of the web page to a file
+    fs.writeFile(process.argv[3], body, 'utf8', function(err) {
+      if (err) {
+        console.error('Error writing to file:', err);
+        return;
+      }
+    });
+  } 
+});
