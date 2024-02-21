f6d2a8763037db3719446e2ffe16786b21aa3a32
Droid <44108804+Effah001@users.noreply.github.com>
2024-02-21 04:28:39 +0000

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -12,7 +12,7 @@ request(apiUrl, function(error, response, body) {
 
   if (response && response.statusCode === 200) {
     const films = JSON.parse(body).results;
-    const idToSearch = 13;
+    const idToSearch = 18;
     const filmsWithId = films.filter(film => film.episode_id === idToSearch);
     console.log(filmsWithId.length);
   } else {
