729a7a64019fc8b24eac124e9dd5ac33ff85f034
Droid <44108804+Effah001@users.noreply.github.com>
2024-02-21 03:24:23 +0000

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,11 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function(err, data) {
-  // Print the file contents
+  // Check for errors
+  if (err) {
+    console.error(err);
+    return;
+  }
+
   console.log(data);
 });
-
